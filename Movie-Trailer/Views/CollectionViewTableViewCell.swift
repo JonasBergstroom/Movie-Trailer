5daabb6c944152500c8330ef69f7b9274f8a5bd9
jonasbergstrom <jontrian97@gmail.com>
2022-11-07 18:41:39 +0100

    Created the "CollectionViewTableViewCell" to be shown at "homeFeedTable"

---
+++ Movie-Trailer/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Movie-Trailer
+//
+//  Created by Jonas Bergström on 2022-11-07.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
