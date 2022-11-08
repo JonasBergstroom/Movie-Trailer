8cf8e1f29af076a90960a3d2ab97cbaf23d4c99f
jonasbergstrom <jontrian97@gmail.com>
2022-11-08 13:44:14 +0100

    Added a Image for the "imageView" with some gradient style

--- Movie-Trailer/Views/MovieHeaderUIView.swift
+++ Movie-Trailer/Views/MovieHeaderUIView.swift
@@ -11,15 +11,26 @@ class MovieHeaderUIView: UIView {
     
     private let movieHeaderImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.image = UIImage(named: "")
+        imageView.image = UIImage(named: "freeGuyImage")
         return imageView
     }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(movieHeaderImageView)
+        addGradient()
     }
     
     override func layoutSubviews() {
